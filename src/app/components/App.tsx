12016203cee5230a0e3e90b192b8648f83628865
Marcello Paiva <marcello.paiva@gmail.com>
2020-12-06 19:44:41 -0500

    organizes components

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,34 +1,175 @@
 import * as React from 'react';
-import {Text} from 'react-figma-plugin-ds';
+import {Button, Checkbox, Input, Label, Select} from 'react-figma-plugin-ds';
 import '../styles/ui.css';
 import 'react-figma-plugin-ds/figma-plugin-ds.css';
 
 declare function require(path: string): any;
 
 const App = ({}) => {
-    React.useEffect(() => {
-        // This is how we read messages sent from the plugin controller
-        window.onmessage = event => {
-            const {type, message} = event.data.pluginMessage;
-            console.log(type, message);
-        };
-    }, []);
+    const [state, setState] = React.useState({
+        granularity: 1,
+        direction: 'VERTICAL',
+        groupSpace: 100,
+        displayTitle: true,
+        spacing: {
+            x: 100,
+            y: 100,
+        },
+        rowMax: '',
+        renameDuplicate: false,
+        zoomCenter: true,
+    });
 
-    // call launchControllerFunctions('message1') to launch the message1 command in src/plugin/controller.ts
-    function launchControllerFunctions(messageType) {
-        parent.postMessage({pluginMessage: {type: messageType}}, '*');
+    React.useEffect(() => {
+        // console.log(state);
+    }, [state]);
+
+    // function launchControllerFunctions(messageType) {
+    //     parent.postMessage({pluginMessage: {type: messageType}}, '*');
+    // }
+
+    function handleChange(event, name) {
+        // console.log(event);
+        setState({
+            ...state,
+            [name]: event,
+        });
+    }
+
+    function handleSpacingChange(event, axis) {
+        // console.log(event);
+        if (axis === 'x') {
+            setState({
+                ...state,
+                spacing: {
+                    x: event,
+                    y: state.spacing.y,
+                },
+            });
+        } else {
+            setState({
+                ...state,
+                spacing: {
+                    y: event,
+                    x: state.spacing.x,
+                },
+            });
+        }
+    }
+
+    function handleSelectChange(event, name) {
+        // console.log(event);
+        setState({
+            ...state,
+            [name]: event.value,
+        });
+    }
+
+    function handleSubmit() {
+        parent.postMessage({pluginMessage: {type: 'organize', state}}, '*');
     }
 
     return (
-        <div id="root">
-            <Text size="xlarge" weight="bold">
-                Welcome to your figma plugin!
-            </Text>
-            <Text size="large" weight="normal">
-                Check out <a href="https://github.com/alexandrtovmach/react-figma-plugin-ds/">react-figma-plugin-ds</a>{' '}
-                on gitHub to learn about the available components!
-            </Text>
-        </div>
+        <form id="root">
+            <Label htmlFor="granularity">Group Granularity</Label>
+            <Input
+                type="number"
+                name="granularity"
+                id="granularity"
+                onChange={e => {
+                    handleChange(e, 'granularity');
+                }}
+                defaultValue={1}
+            />
+            <Label htmlFor="direction">Direction</Label>
+            <Select
+                name="direction"
+                id="direction"
+                onChange={e => {
+                    handleSelectChange(e, 'direction');
+                }}
+                options={[
+                    {value: 'VERTICAL', label: 'Vertical'},
+                    {value: 'HORIZONTAL', label: 'Horizontal'},
+                ]}
+                defaultValue="VERTICAL"
+            />
+            <Label htmlFor="groupSpace">Group Space</Label>
+            <Input
+                type="number"
+                name="groupSpace"
+                id="groupSpace"
+                onChange={e => {
+                    handleChange(e, 'groupSpace');
+                }}
+                defaultValue={100}
+            />
+            <Label htmlFor="spaceX">Spacing X</Label>
+            <Input
+                type="number"
+                name="spaceX"
+                id="spaceX"
+                onChange={e => {
+                    handleChange(e, 'x');
+                }}
+                defaultValue={100}
+            />
+            <Label htmlFor="spaceY">Spacing Y</Label>
+            <Input
+                type="number"
+                name="spaceY"
+                id="spaceY"
+                onChange={e => {
+                    handleSpacingChange(e, 'y');
+                }}
+                defaultValue={100}
+            />
+            <Label htmlFor="rowMax">Maximum Rows</Label>
+            <Input
+                type="number"
+                name="rowMax"
+                id="rowMax"
+                onChange={e => {
+                    handleChange(e, 'rowMax');
+                }}
+                defaultValue={100}
+            />
+            <Checkbox
+                name="displayTitle"
+                id="displayTitle"
+                label="Display Title"
+                onChange={e => {
+                    handleChange(e, 'displayTitle');
+                }}
+                type="checkbox"
+            />
+            <Checkbox
+                name="renameDuplicate"
+                id="renameDuplicate"
+                label="Rename Duplicate Components"
+                onChange={e => {
+                    handleChange(e, 'renameDuplicate');
+                }}
+                type="checkbox"
+            />
+            <Checkbox
+                name="zoomCenter"
+                id="zoomCenter"
+                label="Zoom to Center"
+                onChange={e => {
+                    handleChange(e, 'zoomCenter');
+                }}
+                type="checkbox"
+            />
+            <Button onClick={handleSubmit}>Submit</Button>
+            <Button
+                onClick={() => {
+                    parent.postMessage({pluginMessage: {type: 'showSelection'}}, '*');
+                }}
+            >
+                Show Selection
+            </Button>
+        </form>
     );
 };
 
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,19 +1,65 @@
 figma.showUI(__html__, {
-    height: 250,
+    height: 400,
 });
 
 figma.loadFontAsync({family: 'Roboto', style: 'Regular'});
 
+const clearFill: SolidPaint = {
+    opacity: 0,
+    type: 'SOLID',
+    color: {
+        r: 0,
+        g: 0,
+        b: 0,
+    },
+};
+
 figma.ui.onmessage = msg => {
     switch (msg.type) {
-        case 'message1':
-            //Enter your fimga API code here!!
+        case 'organize':
+            let maxGranularity = 0;
+            let rootFrame = figma.createFrame();
+            let frames = [];
+            let currentPage = figma.currentPage.findAll();
+            let components = currentPage.filter(node => {
+                return node.type === 'COMPONENT';
+            });
+            let componentData = components.map(component => {
+                let data = {
+                    name: component.name,
+                    splitName: component.name.split('/'),
+                    id: component.id,
+                };
+                return data;
+            });
+
+            componentData.forEach(component => {
+                if (maxGranularity < component.splitName.length - 1) {
+                    maxGranularity = component.splitName.length - 1;
+                }
+            });
+
+            if (msg.state.granularity > maxGranularity) {
+                frames = getFrames(componentData, maxGranularity);
+            } else {
+                frames = getFrames(componentData, msg.state.granularity);
+            }
+
+            frames.forEach(frame => {
+                layoutFrame(frame, msg.state);
+                rootFrame.appendChild(frame);
+            });
+
+            rootFrame.fills = [clearFill];
+            rootFrame.layoutMode = msg.state.direction;
+            rootFrame.primaryAxisSizingMode = 'AUTO';
+            rootFrame.counterAxisSizingMode = 'AUTO';
+            rootFrame.itemSpacing = +msg.state.groupSpace;
+
+            figma.closePlugin();
             break;
-        case 'message2':
-            //Enter your fimga API code here!!
-            break;
-        case 'message3':
-            //Enter your fimga API code here!!
+        case 'showSelection':
+            console.log(figma.currentPage.selection);
             break;
         default:
             break;
@@ -22,3 +68,65 @@ figma.ui.onmessage = msg => {
     // figma.closePlugin();
     // Uncomment the line above if you want the plugin to close after running a single message call
 };
+
+function layoutFrame(frame, state) {
+    frame.fills = [clearFill];
+    frame.layoutMode = state.direction === 'VERTICAL' ? 'HORIZONTAL' : 'VERTICAL';
+    frame.primaryAxisSizingMode = 'AUTO';
+    frame.counterAxisSizingMode = 'AUTO';
+    frame.itemSpacing = state.direction === 'VERTICAL' ? +state.spacing.x : +state.spacing.y;
+}
+
+function getFrames(components, granularity) {
+    let frameNames = [];
+    components.forEach(component => {
+        let name = joinName(component.splitName, granularity);
+        if (frameNames.indexOf(name) === -1) {
+            frameNames.push(name);
+        }
+    });
+
+    let frames = [];
+    frameNames.forEach(frameName => {
+        let frameNodes = [];
+        let regex = RegExp(`^${frameName}`);
+
+        components.forEach(component => {
+            if (regex.test(component.name)) {
+                let node = figma.getNodeById(component.id);
+                frameNodes.push(node);
+            }
+        });
+
+        let frame = figma.createFrame();
+        frame.name = frameName;
+
+        frameNodes.forEach(node => {
+            frame.appendChild(node);
+        });
+
+        // const fill: SolidPaint = {
+        //     opacity: 0,
+        //     type: 'SOLID',
+        //     color: {
+        //         r: 0,
+        //         g: 0,
+        //         b: 0,
+        //     },
+        // };
+
+        // frame.fills = [fill];
+
+        frames.push(frame);
+    });
+
+    return frames;
+}
+
+function joinName(splitName, granularity) {
+    let nameArray = splitName;
+    while (nameArray.length > granularity) {
+        nameArray.pop();
+    }
+    return nameArray.join('/');
+}
